426e956d456f1f09a30dc77a8fd8d162e8f56758
zjzjzjzj <zjzjzjzj1874@gmail.com>
2022-03-03 11:14:33 +0800

    feat:✨($opt): add logic operator
    Signed-off-by: zjzjzjzj <zjzjzjzj1874@gmail.com>

---
+++ src/operator/logic.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# 逻辑运算符
+# 逻辑运算符
+# 以下介绍 Shell 的逻辑运算符，假定变量 a 为 10，变量 b 为 20:
+# 注意：逻辑运算符需要两个中括号
+
+# 运算符	 说明	         举例
+# &&	    逻辑的 AND	    [[ $a -lt 100 && $b -gt 100 ]] 返回 false
+# ||	    逻辑的 OR	    [[ $a -lt 100 || $b -gt 100 ]] 返回 true
+
+a=10
+b=20
+
+if [[ ${a} -lt 50 && ${b} -lt 50 ]];then
+    echo "a和b都小于50"
+else 
+    echo "a 和b 不全小于50"
+fi
+
+if [[ ${a} -lt 50 || ${b} -lt 50 ]]
+then
+    echo "a或b小于50"
+else 
+    echo "a和b都不小于50"
+fi
